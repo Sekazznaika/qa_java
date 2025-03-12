2acd7af0b3e9c5046d940c352f112295074f80c4
sekazznaika <dregtar22@Gmail.com>
2025-03-12 18:37:06 +0500

    add jacoco report

---
+++ src/test/java/TestCat.java
@@ -0,0 +1,35 @@
+import com.example.Cat;
+import com.example.Feline;
+import com.example.Predator;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestCat {
+
+    @Mock
+    Feline feline;
+
+
+    @Test
+    public void testGetSound() {
+        Cat cat = new Cat(feline);
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void testGetFood() throws Exception {
+        Cat cat = new Cat(feline);
+        when(feline.eatMeat()).thenReturn(List.of("1", "2"));
+        assertEquals(List.of("1", "2"), cat.getFood());
+    }
+
+}
